13a78fa55dab8c968b30780e8cf57605b727df8d
renb991 <654288522@qq.com>
2024-06-20 23:52:39 +0800

    complete lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -32,8 +32,127 @@ impl ActionKV {
         Ok(ActionKV {f, index} )
     }
 
-    fn process_record<R:Read>(r: &mut Read) -> io::Result<KeyValuePair> {
-        
+    fn process_record<R: Read>(r: &mut R) -> io::Result<KeyValuePair> {
+        let saved_checksum = r.read_u32::<LittleEndian>()?;
+        let key_len = r.read_u32::<LittleEndian>()?;
+        let value_len = r.read_u32::<LittleEndian>()?;
+        let data_len = key_len + value_len;
+
+        let mut data = ByteString::with_capacity(data_len as usize);
+
+        r.by_ref().take(data_len as u64).read_to_end(&mut data)?;
+        debug_assert_eq!(data.len(), data_len  as usize);
+
+        let checksum = crc32::checksum_ieee(&data);
+        if checksum != saved_checksum {
+            panic!(
+                "data corruption encountered ({:08x}) ({:08x})", checksum, saved_checksum
+            );
+        }
+        let value = data.split_off(key_len as usize);
+        let key = data;
+        Ok(KeyValuePair{key, value})
+    }
+
+    fn seek_to_end(&mut self) -> io::Result<u64>{
+        self.f.seek(SeekFrom::End(0))
+    }
+
+    fn load(&mut self) -> io::Result<()>{
+        let mut f = BufReader::new(&mut self.f);
+        loop{
+            let current_pos = f.seek(SeekFrom::Current((0)))?;
+            let maybe_kv = ActionKV::process_record(&mut f);
+            let result_kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => {
+                    match err.kind(){
+                        io::ErrorKind::UnexpectedEof => {
+                            break
+                        },
+                        _ => return Err(err),
+                    }
+                }
+            };
+            self.index.insert(result_kv.key, current_pos);
+        }
+        Ok(())
+    }
+
+    fn get(&mut self, key: &ByteString) -> io::Result<Option<ByteString>> {
+        let pos = match self.index.get(key) {
+            None => return Ok(None),
+            Some(pos) => *pos,
+        };
+        let kv = self.get_at(pos)?;
+        Ok(Some(kv.value))
+    }
+
+    fn get_at(&mut self, pos: u64) -> io::Result<KeyValuePair> {
+        let mut f = BufReader::new(&mut self.f);
+        f.seek(SeekFrom::Start(pos));
+        Ok(ActionKV::process_record(&mut f)?)
+    }
+
+    fn find(&mut self, target: &ByteString) -> io::Result<Option<KeyValuePair>> {
+        let mut f = BufReader::new(&mut self.f);
+        f.seek(SeekFrom::Current(0));
+        loop {
+            let current = ActionKV::process_record(&mut f);
+            let kv = match current {
+                Ok(kv) => {
+                    if kv.value == *target {
+                        return Ok(Some(kv));
+                    }
+                    break;
+                },
+                Err(err) => {
+                    match err.kind() {
+                        io::ErrorKind::UnexpectedEof => return Ok(None),
+                        _ => return Err(err),
+                    }
+                }
+            };
+            
+        }
+        Ok(None)
+    }
+
+    fn insert(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<()>{
+        let pos = self.insert_but_ignore_index(key, value)?;
+        self.index.insert(key.to_vec(), pos);
+        Ok(())
+    }
+
+    fn insert_but_ignore_index(&mut self, key: &ByteStr, value: &ByteStr) -> io::Result<u64>{
+        let mut f = BufWriter::new(&mut self.f);
+        let key_len = key.len();
+        let value_len = value.len();
+        let mut data = ByteString::with_capacity(key_len + value_len);
+        for byte in key {
+            data.push(*byte);
+        }
+        for byte in value {
+            data.push(*byte);
+        }
+        let check_sum = crc32::checksum_ieee(&data);
+
+        let current_pos = f.seek(SeekFrom::End(0))?;
+        f.write_u32::<LittleEndian>(check_sum);
+        f.write_u32::<LittleEndian>(key_len as u32);
+        f.write_u32::<LittleEndian>(value_len as u32);
+        f.write_u32::<LittleEndian>((key_len + value_len) as u32);
+        f.write_all(&data);
+        Ok(current_pos)
+    }
+
+    fn update(&mut self, key: &ByteString, value: &ByteString) -> io::Result<()>{
+        self.insert(key, value)
+    }
+
+    fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
+    
     }
 
 }
